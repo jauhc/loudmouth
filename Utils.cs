61941a1292097494898a301a3ca6153a5bb973ac
jauhc <paperslove420@gmail.com>
2019-10-20 00:37:15 +0300

    added cursed pasta

--- Utils.cs
+++ Utils.cs
@@ -179,7 +179,39 @@ public static class Utils
         return s.Length;
     }
 
+    private static List<string> uwuPasta = new List<string>();
+    private static void cookPasta()
+    {
+        uwuPasta.AddRange(new[] {
+            "rawr x3 nuzzles how are you pounces on you you're so warm ",
+            "o3o notices you have a bulge o: someone's happy ;)",
+            "nuzzles your necky wecky~ murr~ hehehe",
+            "rubbies your bulgy wolgy you're so big :oooo",
+            "rubbies more on your bulgy wolgy it doesn't stop growing ·///·",
+            "kisses you and lickies your necky daddy likies (;",
+            "nuzzles wuzzles I hope daddy really likes $:",
+            "wiggles butt and squirms I want to see your big daddy meat~",
+            "wiggles butt I have a little itch o3o",
+            "wags tail can you please get my itch~",
+            "puts paws on your chest nyea~",
+            "its a seven inch itch rubs your chest can you help me pwease",
+            "squirms pwetty pwease sad face I need to be punished",
+            "runs paws down your chest and bites lip like I need to be punished really good~",
+            "paws on your bulge as I lick my lips I'm getting thirsty",
+            "I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v",
+            "licks shaft mmmm~ so musky drools all over your cock your daddy meat",
+            "I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply",
+            "oh god im so hard~ licks balls punish me daddy~",
+            "nyea~ squirms more and wiggles butt I love your musky goodness",
+            "bites lip please punish me licks lips nyea~",
+            "suckles on your tip so good licks pre of your cock salty goodness~",
+            "eyes role back and goes balls deep mmmm~ moans and suckles"
+        });
+    }
 
+    /// <summary>
+    ///
+    /// </summary>
     private static void chatCommand(string sender, string message)
     {
         if (message.IndexOf("!help") > -1)
@@ -195,11 +227,27 @@ public static class Utils
         {
             if (_testing)
             {
-                echo($"{sender} you have rolled {rng.Next(1, 100)}!");
+                echo($"{sender} you have rolled {rng.Next(1, 100)} (1-100)!");
                 return;
             }
-            owo($"{sender} you have rolled {rng.Next(1, 100)}!");
+            owo($"{sender} you have rolled {rng.Next(1, 100)} (1-100)!");
         }
+        else if (message.IndexOf("hi") > -1)
+            owo(message);
+        else if (message.IndexOf("hey") > -1)
+            owo(message);
+        else if (message.IndexOf("sup") > -1)
+            owo(message);
+        else if (message.IndexOf("hello") > -1)
+            owo(message);
+        else if (message.IndexOf("ty") > -1)
+            owo("np");
+        else if (message.IndexOf("thanks") > -1)
+            owo("no problem");
+        /*
+        else if (message.IndexOf("") > -1)
+            owo("");
+        */
     }
 
     public static string msgCode = "‎ : "; // DONT TOUCH OR WE ALL DIE
@@ -309,6 +357,7 @@ public static class Utils
         if (client.IsConnected) echo("[RCON CONNECTED!!]");
         me = getMyCommunityID();
         InitTimer();
+        cookPasta();
         Thread logger = new Thread((new ThreadStart(rconParser)));
         logger.Start();
         /* 
